3b6c29efcaffe62077f476a5175e5860c7d1fd26
Student User <student@workstation.lab.example.com>
2021-07-25 01:22:53 -0400

    Changed index page contents. with new contain

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,4 @@
 <?php
-print "dkfnkndnvnfkjvdjabfjdbaf Hello, World! php version is " . PHP_VERSION . "\n";
+print "hi to the world of bindra  Hello, World! php version is " . PHP_VERSION . "\n";
+print "A change is a coming!\n";
 ?>
