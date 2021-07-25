c3a721870c28e0cbf9b139fc1dfc7beeb7399cfe
Student User <student@workstation.lab.example.com>
2021-07-25 04:00:35 -0400

    dbasjh

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
-print "hi to the world of bindra  Hello, World! php version is " . PHP_VERSION . "\n";
+print " ajay aaj ghumne jaa rhah h hi to the world of bindra jhughghghj  Hello, World! php version is " . PHP_VERSION . "\n";
 print "A change is a coming!\n";
 ?>
